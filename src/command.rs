21b45bd36e0e8397d3c9801aec619c4bf4b1f661
Ahmed Ibrahim <ib153507@gmail.com>
2022-09-08 05:54:18 +0200

    Split input into program & args

--- src/command.rs
+++ src/command.rs
@@ -8,15 +8,17 @@ pub enum ArgsErr {
     InvalidCommand(String),
     #[error("Found an empty command")]
     EmptyCommand,
+    #[error("At least on command should be passed")]
+    MissingCommandsArg,
 }
 
 /// Run multiple commands concurrently
 pub struct Args {
-    commands: Vec<String>,
+    commands: Vec<process::Command>,
 }
 
 impl Args {
-    pub fn new() {
+    pub fn new() -> Result<(), ArgsErr> {
         let args = clap::Command::new("concurrently")
             .author("Ahmed Ibrahim")
             .version("1.0.0")
@@ -29,28 +31,29 @@ impl Args {
             )
             .get_matches();
 
-        match args.get_many::<String>("commands") {
-            Some(m) => {
-                println!("{:?}", m.collect::<Vec<_>>())
-            }
-            None => {
-                println!("Not found")
-            }
+        let commands_str = args
+            .get_many::<String>("commands")
+            .ok_or(ArgsErr::MissingCommandsArg)?;
+
+        let mut commands = Vec::new();
+        for command in commands_str {
+            let command = Args::parse_command(command)?;
+            commands.push(command);
         }
+
+        Ok(())
     }
 
-    fn parse_command(command: &str) -> Result<(String, Vec<String>), ArgsErr> {
+    fn parse_command(command: &str) -> Result<process::Command, ArgsErr> {
         if command.trim().is_empty() {
             return Err(ArgsErr::EmptyCommand);
         }
 
         let args = command.split_whitespace().collect::<Vec<_>>();
         let program = args.get(0).ok_or(ArgsErr::InvalidCommand(command.into()))?;
-        Ok((
-            program.to_string(),
-            args[1..].iter().map(|a| a.to_string()).collect::<Vec<_>>(),
-        ))
+        let mut command = process::Command::new(*program);
+        command.args(&args[1..]);
+
+        Ok(command)
     }
 }
-
-pub struct CommandBuilder;
